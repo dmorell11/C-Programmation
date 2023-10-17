fb74e54430a4880bbafc7b10f184d346d51f15e9
Daniel M <daniel.morell11@gmail.com>
2023-10-17 23:24:53 +0200

    Enunciado Ejercicio 3

---
+++ Ejercicio 3/Ejercicio 3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+    /*
+    Ejercicio: Creación de una clase de Productos
+
+    Crea una clase llamada Producto que represente un producto en una tienda. 
+    La clase Producto deberá tener los siguientes atributos privados:
+
+    nombre (string): el nombre del producto.
+    precio (double): el precio del producto.
+    cantidadEnStock (int): la cantidad de unidades del producto en stock.
+
+    Debes proporcionar métodos públicos para establecer y obtener estos atributos. 
+    Utiliza propiedades para encapsular el acceso a los atributos y asegúrate de que 
+    el precio sea un valor positivo y que la cantidad en stock sea mayor o igual a cero.
+
+    Además, la clase Producto debe tener un método llamado CalcularValorTotal que calcule 
+    el valor total en stock del producto multiplicando el precio por la cantidad en stock.
+
+    Crea una instancia de la clase Producto, establece sus atributos y luego llama al 
+    método CalcularValorTotal para calcular el valor total en stock del producto y 
+    mostrarlo en la consola.
+
+    Asegúrate de que tu código esté bien estructurado y que se utilicen propiedades 
+    y métodos de manera apropiada para acceder y modificar los atributos de la clase Producto.
+    */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
